579210750a91fed9aa7927091eaa5ce26dbd14f3
Volkan Abaoglu <volkan.abaoglu>
2024-05-11 23:20:25 +0300

    initial commit

---
+++ src/screens/RecentExpenses.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RecentExpenses() {
+  return (
+    <View>
+      <Text>RecentExpenses</Text>
+    </View>
+  )
+}
