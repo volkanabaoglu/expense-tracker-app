075414893389336df7b0c5e4ace689ee5544cd56
Volkan Abaoglu <volkan.abaoglu>
2024-05-12 01:18:46 +0300

    feat: add navigation and bottom tab bar for expense overview
    This commit adds navigation functionality and a bottom tab bar to the expense overview screen. It uses the `@react-navigation/native` and `@react-navigation/bottom-tabs` packages to implement the navigation and tab bar components. The `ExpenseOverview` component is created to render the bottom tab navigator with two screens: `AllExpenses` and `RecentExpenses`. The header and tab bar styles are also customized to match the app's design using the `GlobalStyles` constants.

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import AllExpenses from './src/screens/AllExpenses';
+import ManageExpense from './src/screens/ManageExpense';
+import RecentExpenses from './src/screens/RecentExpenses';
+import { GlobalStyles } from './src/constants/styles';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+
+function ExpenseOverview() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BottomTabs.Navigator screenOptions={{
+      headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      headerTintColor: '#fff',
+      tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+      tabBarActiveTintColor: { color: GlobalStyles.colors.accent500 },
+    }}>
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+    </BottomTabs.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default function App() {
+  return (
+    <>
+
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Stack.Navigator>
+    
+          <Stack.Screen name="ExpenseOverview" component={ExpenseOverview} options={{headerShown:false}}  />
+          <Stack.Screen name="MangeExpense" component={ManageExpense} />
+
+        </Stack.Navigator>
+      </NavigationContainer>
+
+    </>
+
+  );
+}
+
